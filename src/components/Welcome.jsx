4032ae89df5537134813d081599da4822cc1723d
Greg Cain <codegregcode@gmail.com>
2023-11-17 18:07:24 +0000

    creates and writes Welcome compoent to pass tests

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div>
+      {' '}
+      <p>Thank you for showing an interest in our service.</p>
+      <p>
+        Before we send you the postage labels we would like to ask you a few
+        questions regarding the items you were wishing to sell
+      </p>
+      <p>Click here to get started</p>
+    </div>
+  );
+};
+
+export default Welcome;
