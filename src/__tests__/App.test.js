65ead1e5ec2fffb8acc48cf2d0ab8187c9362093
Greg Cain <codegregcode@gmail.com>
2023-11-17 17:03:42 +0000

    updates App test with describe and it

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { React, render, screen } from '@testing-library/react';
 import App from '../components/App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders h1 title', () => {
+    render(<App />);
+    const h1Element = screen.getByText(/sell us your stuff/i);
+    expect(h1Element).toBeInTheDocument();
+  });
 });
