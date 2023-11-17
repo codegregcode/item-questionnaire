2b658b82fc1513535148881035af77faa5e00f9c
Greg Cain <codegregcode@gmail.com>
2023-11-17 18:05:27 +0000

    adds beforeEach to render App for tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,8 +2,11 @@ import { React, render, screen } from '@testing-library/react';
 import App from '../components/App';
 
 describe('App', () => {
-  it('renders h1 title', () => {
+  beforeEach(() => {
     render(<App />);
+  });
+
+  it('renders h1 title', () => {
     const h1Element = screen.getByText(/sell us your stuff/i);
     expect(h1Element).toBeInTheDocument();
   });
