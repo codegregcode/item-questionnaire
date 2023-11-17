c290f8a6654b8e250d68b9e8230baa80211f6c88
Greg Cain <codegregcode@gmail.com>
2023-11-17 18:11:11 +0000

    imports Welcome component into App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Welcome from './Welcome';
 import '../styles/app.css';
 
 const App = () => (
   <div className="App">
     <h1>Sell us your stuff</h1>
+    <Welcome />
   </div>
 );
 
