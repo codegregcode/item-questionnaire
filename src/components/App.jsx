d40d9f64323ede6865f515ad76e7d55186d62092
Greg Cain <codegregcode@gmail.com>
2023-11-18 15:39:52 +0000

    imports Route and Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Welcome from './Welcome';
 import '../styles/app.css';
 
 const App = () => (
   <div className="App">
     <h1>Sell us your stuff</h1>
-    <Welcome />
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+    </Routes>
   </div>
 );
 
