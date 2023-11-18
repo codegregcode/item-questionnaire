f7e219e495de348180d9e68c7d5bb2fa93749f11
Greg Cain <codegregcode@gmail.com>
2023-11-18 17:01:33 +0000

    imports and adds Route path for SignUp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Welcome from './Welcome';
+import SignUp from './SignUp';
 import '../styles/app.css';
 
 const App = () => (
@@ -8,6 +9,7 @@ const App = () => (
     <h1>Sell us your stuff</h1>
     <Routes>
       <Route path="/" element={<Welcome />} />
+      <Route path="/sign-up" element={<SignUp />} />
     </Routes>
   </div>
 );
