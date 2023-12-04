a2a5cfc9805aee8869c7fae76f27fe5e8316c34a
Greg Cain <codegregcode@gmail.com>
2023-12-04 18:34:18 +0000

    centers form and imports style to SignUp component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,22 +1,47 @@
 import React from 'react';
+import '../styles/signup.css';
 
 const SignUp = () => {
   return (
-    <div>
+    <>
       <p>
         Before you tell us about your items, we need to collect a few details
         from yourself
       </p>
-      <div className="register__container">
-        <input type="text" placeholder="Full Name" />
-        <input type="number" placeholder="Mobile Phone" />
-        <input type="number" placeholder="Landline" />
-        <input type="text" placeholder="Address & Post Code" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Register</button>
+      <div className="signup">
+        <div className="signup__container">
+          <input
+            className="signup__textBox"
+            type="text"
+            placeholder="Full Name"
+          />
+          <input
+            className="signup__textBox"
+            type="number"
+            placeholder="Mobile Phone"
+          />
+          <input
+            className="signup__textBox"
+            type="number"
+            placeholder="Landline"
+          />
+          <input
+            className="signup__textBox"
+            type="text"
+            placeholder="Address & Post Code"
+          />
+          <input className="signup__textBox" type="email" placeholder="Email" />
+          <input
+            className="signup__textBox"
+            type="password"
+            placeholder="Password"
+          />
+          <button className="signup__textBox" type="submit">
+            Sign Up
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
