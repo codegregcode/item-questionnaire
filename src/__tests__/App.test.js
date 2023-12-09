d5e04abdffa96d10bb45c2ba3031cacdd06d2e25
Greg Cain <codegregcode@gmail.com>
2023-12-09 17:59:46 +0000

    creates snapshot test for App

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,18 +1,16 @@
-import { React, render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from '../components/App';
 
 describe('App', () => {
-  beforeEach(() => {
-    render(
+  it('renders correctly', () => {
+    const { asFragment } = render(
       <MemoryRouter initialEntries={['/']}>
         <App />
       </MemoryRouter>
     );
-  });
 
-  it('renders h1 title', () => {
-    const h1Element = screen.getByText(/sell us your stuff/i);
-    expect(h1Element).toBeInTheDocument();
+    expect(asFragment()).toMatchSnapshot();
   });
 });
