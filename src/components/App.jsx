729af77735f586fbe14feb365ebe4c988a7b2ec2
Greg Cain <codegregcode@gmail.com>
2023-12-09 18:22:42 +0000

    imports Header into App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Welcome from './Welcome';
 import SignUp from './SignUp';
+import Header from './Header';
 import '../styles/app.css';
 
 const App = () => (
-  <div className="App">
-    <h1>Sell us your stuff</h1>
-    <Routes>
-      <Route path="/" element={<Welcome />} />
-      <Route path="/sign-up" element={<SignUp />} />
-    </Routes>
+  <div className="background">
+    <div className="App">
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </div>
+    </div>
   </div>
 );
 
