9aa1109781d92b151f4c2d34c0d39a244b28ec78
Greg Cain <codegregcode@gmail.com>
2023-12-20 18:49:14 +0000

    creates ItemList component and styles

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/item-list.css';
+
+const ItemList = () => {
+  return (
+    <div>
+      <p>
+        Let&apos;s find out a bit more about the items you&apos;re interested in
+        selling to us
+      </p>
+      <p>
+        Please take your time to go through the list and add items to your box
+        (just click add to box - you can also add notes about the items)
+      </p>
+      <p>Here&apos;s a list of all the items that we accept:</p>
+      <div className="item-list">
+        {/* this is where the <Item /> will load */}
+        <p>items</p>
+      </div>
+    </div>
+  );
+};
+
+export default ItemList;
