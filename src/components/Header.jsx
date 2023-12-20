5c195c5657f123e6fd940e7dbb6ff05c84ce1c65
Greg Cain <codegregcode@gmail.com>
2023-12-20 17:40:17 +0000

    updates styling on different devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,7 @@ const Header = () => {
     <>
       <div className="container" data-testid="svg-container">
         <svg viewBox="0 0 500 250" preserveAspectRatio="xMinYMin meet">
-          <path
-            d="M0,50 C150,100 350,0 500,50 L500,0 L0,0 Z"
-            style={{ stroke: 'none', fill: '#514F9E' }}
-          />
+          <path d="M0,50 C150,100 350,0 500,50 L500,0 L0,0 Z" />
         </svg>
       </div>
       <div className="title">
