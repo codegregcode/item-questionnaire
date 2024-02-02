f5ceafbc7b3dfeb1cfafbd2943614e313152f6e9
Greg Cain <codegregcode@gmail.com>
2024-02-02 17:28:53 +0000

    adds auth, login, signout, reset functions

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,37 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  where,
+  doc,
+  setDoc,
+} from 'firebase/firestore';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+} from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
 const getItems = async () => {
-  const querySnapshot = await db.collection('items').get();
+  const q = query(collection(db, 'items'));
+  const querySnapshot = await getDocs(q);
 
   const items = [];
   querySnapshot.forEach((i) => {
@@ -30,8 +41,51 @@ const getItems = async () => {
   return items;
 };
 
-const signInWithGoogle = () => {
-  return 1;
+const registerWithEmailAndPassword = async (
+  name,
+  email,
+  password,
+  address,
+  landline,
+  mobile
+) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const { user } = res;
+    const userRef = doc(db, 'users', user.uid);
+    await setDoc(userRef, {
+      uid: user.uid,
+      name,
+      authProvider: 'local',
+      email,
+      address,
+      landline,
+      mobile,
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
-export { getItems, signInWithGoogle };
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+const reset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    console.log('Password reset link sent!');
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+export { getItems, registerWithEmailAndPassword, logout, login, reset, auth };
