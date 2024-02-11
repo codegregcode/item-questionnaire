5e850a6dd6fa5a76004272320d288b2825ee1803
Greg Cain <codegregcode@gmail.com>
2024-02-11 14:54:27 +0000

    imports browser router and wraps component

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from '../components/Header';
 
 describe('Header', () => {
   it('renders Header component correctly', () => {
-    render(<Header />);
+    render(
+      <Router>
+        <Header />
+      </Router>
+    );
     expect(screen.getByAltText('logo')).toBeInTheDocument();
     expect(screen.getByTestId('svg-container')).toBeInTheDocument();
   });
